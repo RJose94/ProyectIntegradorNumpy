6a112102c9a67d834eeab5792cb0a9047c8d16a4
RJose94 <137239149+RJose94@users.noreply.github.com>
2023-09-10 17:30:53 -0500

    Correcion

--- parte1_NumPy.py
+++ parte1_NumPy.py
@@ -1,17 +1,12 @@
-# Importar las bibliotecas necesarias
 from datasets import load_dataset
 import numpy as np
 
-# Descargar el conjunto de datos
 dataset = load_dataset("mstz/heart_failure")
 
-# Acceder a la partición de entrenamiento
 data = dataset["train"]
 
-# Convertir la lista de edades a un arreglo de NumPy
 edades = np.array(data['age'])
 
-# Calcular el promedio de edad
 promedio_edad = np.mean(edades)
 
 print(f"El promedio de edad de las personas participantes en el estudio es {promedio_edad}")
